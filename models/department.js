8318ed7906027decc6bcb5112f6f6945120aaebd
Austin Luper <austin.luper@gmail.com>
2024-03-29 22:38:05 -0400

    Added pseudocode into the department.js file

---
+++ models/department.js
@@ -0,0 +1,7 @@
+/* View all departments in the department table*/
+
+/* Add a new department to the department table*/
+
+/* Delete a department from the department table*/
+
+/* View all employees within a department in the department table*/
