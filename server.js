275bb544e6f9a1a56a47d108475a779b0fc36e37
Austin Luper <austin.luper@gmail.com>
2024-03-30 12:21:48 -0400

    Reordered the requirements of packages in the server.js file and included the requirement of express

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-// Import and require inquirer
-const inquirer = require('inquirer');
+// Import and require express
+const express = require('express');
 
 // Import and require mysql2
 const mysql = require('mysql2');
+
+// Import and require inquirer
+const inquirer = require('inquirer');
 
 inquirer
   .prompt([
