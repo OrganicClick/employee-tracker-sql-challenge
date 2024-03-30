5221703deef8ea4f67ea9389c6629c85c4640e2b
Austin Luper <austin.luper@gmail.com>
2024-03-30 12:28:07 -0400

    Added generic express middleware logic to the server.js file which will need to be filled with code more specific to this application. Also commented out placeholder inquirer logic within server.js file

--- server.js
+++ server.js
@@ -7,11 +7,31 @@ const mysql = require('mysql2');
 // Import and require inquirer
 const inquirer = require('inquirer');
 
-inquirer
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '',
+    database: 'employeeTracker_db'
+  },
+  console.log(`Connected to the employeeTracker_db database.`)
+);
+
+/* inquirer
   .prompt([
     /* Pass your questions in here */
-  ])
-  .then((answers) => {
+  /*])
+  /* .then((answers) => {
     // Use user feedback for... whatever!!
   })
   .catch((error) => {
@@ -20,4 +40,14 @@ inquirer
     } else {
       // Something else went wrong
     }
-  });
+  }); */
+
+
+  // Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
