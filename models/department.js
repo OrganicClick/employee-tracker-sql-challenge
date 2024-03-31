e47add2c9a50b212e53bd4d345997e20aaa2a95f
Austin Luper <austin.luper@gmail.com>
2024-03-31 19:30:32 -0400

    Copied functional viewDepartments method into department.js file for later modularization of code

--- models/department.js
+++ models/department.js
@@ -1,12 +1,4 @@
 /* View all departments in the department table*/
-
-/* Add a new department to the department table*/
-
-/* Delete a department from the department table*/
-
-/* View all employees within a department in the department table*/
-
-// Function to handle viewing departments
 async function viewDepartments() {
     try {
       const [rows] = await db.promise().query('SELECT * FROM department');
@@ -16,6 +8,12 @@ async function viewDepartments() {
       console.error('Error viewing department table:', error);
     }
   }
+/* Add a new department to the department table*/
+
+/* Delete a department from the department table*/
+
+/* View all employees within a department in the department table*/
+
 
   // Function to handle adding a department
   async function addDepartment() {
