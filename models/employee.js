d5471a0648c167069960bbcadb8f578f85b4a7a9
Austin Luper <austin.luper@gmail.com>
2024-03-31 20:40:27 -0400

    Added additional pseudocode to employee.js and role.js files

--- models/employee.js
+++ models/employee.js
@@ -1,3 +1,7 @@
-/* Additional switch statement or logic to handle additions of employees,
+/* viewEmployees. This will be to view ALL EMPLOYEES within the entire db */
+
+/* addEmployee. Additional switch statement or logic to handle additions of employees,
 like additional prompts for user to enter employee first name, last name, role id,
 and manager id. */
+
+/* deleteEmployee. Needs to be a menu selection for user to select which employee to delete.*/
---
+++ models/role.js
@@ -0,0 +1,7 @@
+/* viewRoles. This will allow user to view ALL ROLES within the database. */
+
+/* addRole. Needs to have user prompts/menu selection for title of the new role, salary of the role, and department 
+that role will belong to */
+
+
+/* deleteRole. Needs to have user menu selection for which role they want to delete   */
